e38ea879b0793ddcb7e4caab1d590f500e6f4e4d
Jean Vengoechea <inv.novatech@gmail.com>
2024-02-14 23:10:02 -0500

    arreglos finales

--- js/index.js
+++ js/index.js
@@ -1,59 +1,101 @@
-/* 1. Mensaje de bienvenida*/
-alert("Bienvenido al Banco Central")
-
-/* 2. Consulta de salario */
-const salario = prompt(`Por favor ingrese su salario`)
-Number(salario)
-console.log(`Su salario es de $${salario}`)
-
-let cuotaIdeal = salario * 0.20
-let interesMinimo = 2
-let cuotasMinimas = 12
-
-function prestamoMaximo(prestamo, cuota, interes) {
-    prestamoIdeal = (prestamo * cuota) * interes
+// Orden de Datos del cliente
+class cliente {
+    constructor(nombre, edad, trabajo, salario, antiguedad, deudas) {
+        this.nombre = nombre;
+        this.edad = parseFloat(edad);
+        this.trabajo = trabajo;
+        this.salario = parseFloat(salario);
+        this.antiguedad = antiguedad.toLowerCase();
+        this.deudas = deudas.toLowerCase();
+    }
 }
-prestamoMaximo(cuotaIdeal, cuotasMinimas, interesMinimo)
-console.log(`Tu prestamo ideal es $${prestamoIdeal}`)
 
-/* 3. Consulta de prestamo */
-let prestamoSolicitado = prompt("Por favor ingrese valor de prestamo a solicitar")
+// Datos del cliente
+alert(`Bienvenido al Banco Del Mundo (BDM)`)
+
+const clienteNuevo = new cliente(prompt(`Ingrese su nombre`), prompt(`Ingrese su edad`), prompt(`Ingrese su trabajo`), prompt(`Ingrese su salario`), prompt(`¿Usted abrio su cuenta con nosotros hace 5 años o mas?`), prompt(`¿Usted tiene deudas pendientes de pago?`))
+
+
+alert(`Su nombre es ${clienteNuevo.nombre}
+Su edad es ${clienteNuevo.edad} años
+Su trabajo es "${clienteNuevo.trabajo}"
+Su salario es de $${clienteNuevo.salario}
+¿Tienes una antiguedad mayor a 5 años en el banco?: ${clienteNuevo.antiguedad}
+¿Tienes deudas pendientes?: ${clienteNuevo.deudas}`)
+
+// Calculo de interes base
+const intereses = [1.20, 1.30, 1.50]
+
+let interesBase = 0
+
+if (clienteNuevo.antiguedad == `si` && clienteNuevo.deudas == `no`) {
+    interesBase = intereses[0]
+} else if (clienteNuevo.antiguedad == `no` && clienteNuevo.deudas == `no`) {
+    interesBase = intereses[1]
+} else {
+    interesBase = intereses[2]
+}
+
+// Calculo de prestamo maximo
+let valorPrestamoMaximo = clienteNuevo.salario * 0.20
+let cuotasPrestamoMaximo = 12
+
+function prestamoIdeal(prestamo, cuota, interes) {
+    prestamoMaximo = (prestamo * cuota) * interes
+}
+
+prestamoIdeal(valorPrestamoMaximo, cuotasPrestamoMaximo, interesBase)
+
+alert(`El monto maximo de prestamo que podes solicitar es de $${prestamoMaximo}`)
+
+// Solicitud de prestamo
+let prestamoSolicitado = prompt(`¿Cuanto deseas pedir de prestamo?`)
+
 Number(prestamoSolicitado)
-console.log(`Tu prestamo solicitado es $${prestamoSolicitado}`)
 
+alert(`Tu prestamo solicitado es $${prestamoSolicitado}`)
 
-while (prestamoSolicitado > prestamoIdeal) {
-    prestamoSolicitado = prompt(`Tu prestamo es mayor al maximo que puedes pedir en base a tu sueldo.
-    Por favor ingresa un nuevo monto de prestamo` )
+while (prestamoSolicitado > prestamoMaximo) {
+    prestamoSolicitado = prompt(`Tu prestamo excede el maximo permitido con base a tu sueldo.
+    Por favor ingresa un nuevo monto a prestar` )
 }
 
-/* 4. Consultar cantidad de cuotas */
-let cuotasSolicitadas = prompt("Porfavor ingrese el numero de cuotas solicitadas")
+Number(prestamoSolicitado)
+
+alert(`Tu prestamo solicitado es $${prestamoSolicitado}`)
+
+// Solicitud de cuotas
+let cuotasSolicitadas = prompt(`¿En cuantas cuotas quiere pedir su prestamo?`)
+
 Number(cuotasSolicitadas)
-console.log(`Las cuotas solicitadas para el prestamo son ${cuotasSolicitadas} cuotas`)
 
-/* 5. Calcular el interes del prestamo en base a las cuotas solicitadas */
+alert(`Tus cuotas solicitadas son ${cuotasSolicitadas} cuotas`)
 
-let interes = 1.3
+// Calculo de interes de cuotas
+const interesCuotas = [1.50, 1.75, 2.25, 2.75, 3]
+
+let interesCuotasSolicitadas = 0
 
 if (cuotasSolicitadas <= 12) {
-    interesPrestamo = 1.3
+    interesCuotasSolicitadas = interesCuotas[0]
 } else if (cuotasSolicitadas > 12 && cuotasSolicitadas <= 24) {
-    interesPrestamo = 1.5
+    interesCuotasSolicitadas = interesCuotas[1]
 } else if (cuotasSolicitadas > 24 && cuotasSolicitadas <= 36) {
-    interesPrestamo = 1.7
+    interesCuotasSolicitadas = interesCuotas[2]
 } else if (cuotasSolicitadas > 36 && cuotasSolicitadas <= 48) {
-    interesPrestamo = 1.9
+    interesCuotasSolicitadas = interesCuotas[3]
 } else {
-    interesPrestamo = 2
+    interesCuotasSolicitadas = interesCuotas[4]
 }
 
-/* 6. Calculo de valor */
-let valorFinalPrestamoSolicitado = 0
-function valorPrestamoSolicitado(prestamo, interes) {
-    valorFinalPrestamoSolicitado = prestamo * interes
+alert(`El interes que aplica para las ${cuotasSolicitadas} cuotas es de un ${interesCuotasSolicitadas}% sobre el prestamo solicitado`)
+
+// Calculadora de cuotas de prestamo
+function calculadoraDeCuotas(prestamo, cuotas, interes) {
+    cuotasPrestamo = (prestamo / cuotas) * interes
 }
-valorPrestamoSolicitado(prestamoSolicitado, interesPrestamo, cuotasSolicitadas)
-let valorDeCuotas = valorFinalPrestamoSolicitado / cuotasSolicitadas
-alert((`El valor final del prestamo solicitado es de $${valorFinalPrestamoSolicitado} y se solicita en ${cuotasSolicitadas} `))
-alert((`El valor final de las cuotas es $${valorDeCuotas}`))
+calculadoraDeCuotas(prestamoSolicitado, cuotasSolicitadas, interesCuotasSolicitadas)
+
+alert(`El prestamo solicitado es de $${prestamoSolicitado}, la cantidad de cuotas es de ${cuotasSolicitadas}, el interes que se aplica sobre el prestamo es de ${interesCuotasSolicitadas}% y el valor de la cuota mensual y consecutiva es de $${cuotasPrestamo}`)
+
+alert(`Proceso Terminado. Vuelva Pronto 😎`)
